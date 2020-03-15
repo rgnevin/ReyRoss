4f473a1ca992273103272b9a72d1e431d20b7ed0
Ross Nevin <rossnevin@Ross-MacBook-Pro-2.local>
2020-03-15 12:02:17 -0500

    change public capitalization

--- app.js
+++ app.js
@@ -2,16 +2,15 @@ var express = require("express");
 var app = express();
 var path = require("path");
 
-app.use(express.static('ReyRoss'));
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'Public')));
 app.set('view engine', 'html');
 
 app.get("/", function(req,res){
-    res.sendFile(path.join(__dirname+'/RRHome.html'));
+    res.sendFile('public/index.html', { root: __dirname });
 });
 
-// app.listen(3000, function(req,res){
-//     console.log("server started");
-// });
+app.listen(3000, function(req,res){
+    console.log("server started");
+});
 
-app.listen(process.env.PORT, process.env.IP);
+// app.listen(process.env.PORT, process.env.IP);
