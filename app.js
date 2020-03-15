0dd6d1bfa13e6f56cf91cc3e5e005a541dc81ea6
Ross Nevin <rossnevin@Ross-MacBook-Pro-2.local>
2020-03-15 12:05:40 -0500

    change listen command 4server

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ app.get("/", function(req,res){
     res.sendFile('public/index.html', { root: __dirname });
 });
 
-app.listen(3000, function(req,res){
-    console.log("server started");
-});
+// app.listen(3000, function(req,res){
+//     console.log("server started");
+// });
 
-// app.listen(process.env.PORT, process.env.IP);
+app.listen(process.env.PORT, process.env.IP);
